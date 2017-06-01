7462a968adbcad3c55154e59174417dcd911d7f9
Nik Bornemeier <NikolausRB@gmail.com>
2017-06-01 12:35:53 -0600

    create first test for Weather.js

---
+++ test/Weather.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Weather from '../lib/Weather';
+
+
+describe('Weather', () => {
+  it('should render', () => {
+    expect(true);
+  })
+  
+})

b78d95acd136c5b99cb9b59e1ed555abcad4a0a8
Nik Bornemeier <NikolausRB@gmail.com>
2017-06-01 21:04:10 -0600

    add additionial weather condition icons, style main section, and hourly and daily cards

--- lib/components/CurrentForecast/index.jsx
+++ lib/components/CurrentForecast/index.jsx
@@ -10,16 +10,20 @@ const CurrentForecast = ({ weather, location }) => {
     <header  className='current-header'>
       <article className='current-temperatures'>
         <button className='change-city-btn'>Change City</button>
+        <h2 className='temp high-temp'>High: {weather.high}˚F</h2>
         <h1 className='current-temp'>{weather.temp}˚F</h1>
-        <h2 className='temp'>High: {weather.high}˚F</h2>
         <h2 className='temp'>Low: {weather.low}˚F</h2>
       </article>
+
+
       <article className='current-main'>
         <h1 className='current-location'>{location}</h1>
-        <h1 className='current-date'>May 27th, 2017</h1>
-        <img src={IconsImagesObj[weather.icon]}></img>
+        <h1 className='current-date'>June 2nd, 2017</h1>
+        <img className='current-icon' src={IconsImagesObj[weather.icon]}></img>
         <h1 className='current-weather'>{weather.weather}</h1>
       </article>
+
+
       <article className='current-summary-article'>
         <h2 className='current-summary'>Summary: {weather.summary}</h2>
       </article>

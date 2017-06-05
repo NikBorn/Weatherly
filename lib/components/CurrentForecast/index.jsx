2c5b520d2c4b7e5602c957864772e5c96653e34e
Nik Bornemeier <NikolausRB@gmail.com>
2017-06-05 09:24:49 -0600

    redo images, fix spacing on current forecast

--- lib/components/CurrentForecast/index.jsx
+++ lib/components/CurrentForecast/index.jsx
@@ -7,24 +7,32 @@ const CurrentForecast = ({ weather }) => {
   const { high, low, temp, city, icon, currentWeather, summary, date } = weather;
 
   return (
-    <header className='current-header'>
-      <article className='current-temperatures'>
-        <h2 className='temp high-temp'>High: { high }˚F</h2>
-        <h1 className='current-temp'>{ temp }˚F</h1>
-        <h2 className='temp'>Low: { low }˚F</h2>
-      </article>
-
-      <article className='current-main'>
+    <header>
+      <article className='current-location-container'>
         <h1 className='current-location'>{ city }</h1>
         <h1 className='current-date'>{ date }</h1>
-        <img className='current-icon' src={ IconsImagesObj[icon] }></img>
-        <h1 className='current-weather'>{ currentWeather }</h1>
       </article>
 
-      <article className='current-summary-article'>
-        <h2 className='current-summary'>CURRENT FORECAST:</h2>
-        <h2 className='current-summary'>{ summary }</h2>
+      <article className='current-header'>
+
+          <article className='current-temperatures'>
+            <h1 className='temp'>Current Temp</h1>
+            <h1 className='current-temp'>{ temp }˚F</h1>
+            <h2 className='temp'>Low/High:</h2>
+            <h2 className='temp'>{ low }˚F/{ high }˚F</h2>
+          </article>
+
+          <article className='current-main'>
+            <img className='current-icon' src={ IconsImagesObj[icon] }></img>
+            <h1 className='current-weather'>{ currentWeather }</h1>
+          </article>
+
+          <article className='current-summary-article'>
+            <h2 className='current-summary current-summary-title'>CURRENT FORECAST:</h2>
+            <h2 className='current-summary'>{ summary }</h2>
+          </article>
       </article>
+
     </header>
   );
 };

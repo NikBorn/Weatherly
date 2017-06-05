e1057cdc8760ee4d6394c7bf9f053f74fca214d4
coleworsley <cole.s.worsley@gmail.com>
2017-06-04 21:54:52 -0600

    add autocomplete functionality

--- lib/components/SearchBar/SearchBar.jsx
+++ lib/components/SearchBar/SearchBar.jsx
@@ -1,30 +1,36 @@
 import React, { Component } from 'react';
 import './SearchBar.css';
-
+import Trie from '@coleworsley/complete-me';
+import citiesList from '../../citiesList';
 
 export default class SearchBar extends Component {
   constructor() {
     super();
     this.state = {
       input: '',
+      trie: {},
     };
   }
 
-  // findCity() {
-  //
-  // }
-  //
-  // findState() {
-  //
-  // }
-  //
-  // findZip() {
-  //
-  // }
-  //
-  // determineAPISearch() {
-  //
-  // }
+  componentWillMount() {
+    const newTrie = new Trie();
+    newTrie.populate(citiesList);
+    this.setState({ trie: newTrie });
+  }
+
+  autocomplete(str) {
+    const suggestions = this.state.trie.suggest(str);
+
+    if (suggestions.length > 5) {
+      suggestions.splice(5, suggestions.length);
+    }
+
+    return suggestions.map(e => (
+      <li key={e} className='autocomplete-item'>
+        {e}
+      </li>
+    ));
+  }
 
   render() {
     const { input } = this.state;
@@ -39,8 +45,12 @@ export default class SearchBar extends Component {
             this.setState({ input: e.target.value });
           }}/>
         <button className='search-btn search-element'
-                onClick={() => changeLocation(input)}
-          >Get Weather</button>
+                onClick={() => changeLocation(input)}>
+                Get Weather
+        </button>
+        <ul className='autocomplete-list'>
+          {this.autocomplete(this.state.input)}
+        </ul>
       </div>
     );
   }
